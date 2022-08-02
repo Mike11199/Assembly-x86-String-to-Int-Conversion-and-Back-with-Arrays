209208b91dbc24f5800a8fa59d0fa99b0af94110
Michael <miwanek11199@gmail.com>
2022-08-01 19:29:11 -0700

    added things

--- Proj6_iwanekm.asm
+++ Proj6_iwanekm.asm
@@ -40,11 +40,12 @@ ENDM
 program_info_1		BYTE		"Hello!  Welcome to my program:  String Primitives and Macros by Michael Iwanek",13,10,13,10,0
 program_info_2		BYTE		"Please enter in 10 signed decimal integers.  This program will then display each number entered, their average value, and sum.",13,10
 					BYTE		"Each number must be able to fit within a 32 bit register, or be between the values of -2,147,483,647 and 2,147,483,647 inclusive (or +/- 2^31).",13,10,13,10,0
-userString			BYTE		10 DUP(0)
+userString			BYTE		12 DUP(0)			;10 digit string, +1 for + or neg sign; +1 for null terminator
 userString_len		DWORD		LENGTHOF userString
-num_prompt			BYTE		"Please enter a signed number: ",0
-
-
+num_prompt			BYTE		"Please enter a signed number between -2^31 and 2^31: ",0
+IntegerArray		SDWORD		2000 DUP(?)
+IntegerArray_len	DWORD		LENGTHOF IntegerArray  ;num elements
+IntegerArray_size	DWORD		SIZEOF IntegerArray	   ;num bytes
 
 .code
 main PROC
@@ -53,6 +54,12 @@ main PROC
 	
 	mDisplayString OFFSET program_info_1
 	mDisplayString OFFSET program_info_2
+	
+
+	PUSH    userString_len
+	PUSH	OFFSET userString
+	PUSH	OFFSET num_prompt
+	CALL	ReadVal
 
 
 	Invoke ExitProcess,0	; exit to operating system
@@ -61,8 +68,29 @@ main ENDP
 
 ReadVal PROC
 
-	mGetString OFFSET userString, OFFSET userString_len
+	;Invoke the mGetString macro to get user input in the form of a string of digits
+	
+	LOCAL Value:DWORD, wholeNum:DWORD
+	mDisplayString [EBP + 8]				;prompt num	
+	mGetString [EBP + 12], [EBP + 16]		;pass (ref, size) to macro to get user string
 
+
+
+
+	;Convert (USING STRING PRIMITIVES) the string of ASCII digits to its numeric value representation (SDWORD), validating each char is a valid # (not symbol)
+	
+	;LODSB	;takes ESI and copies to AL, then increment ESI to next element
+
+	mov EDX, OFFSET userString		;test delete
+	CALL WriteString				;test delete
+
+
+	;Store this one value in a memory variable (output paratmeter, by reference).
+
+
+
+
+	RET 4		; dereference 1 passed parameter address
 
 ReadVal ENDP
 
